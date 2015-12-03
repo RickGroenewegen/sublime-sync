4de3c2f60b1e4ca0145fd135809501fd31875c8d
Rick <rick@aanzee.nl>
2015-12-03 09:25:12 +0100

    Added manual sftp-config.json generation for non-sublime projects. Added password storage in keychain for Mac

--- index.js
+++ index.js
@@ -6,10 +6,79 @@ var SFTPS = require('sftps');
 var RJSON = require('relaxed-json');
 var util = require('util');
 var colors = require('colors');
+var keychain = require('keychain');
+var prompt = require('prompt');
+
 var content = '';
 var settings = require('./package.json');
 var jobs = [];
 var scanInterval = 1000;
+var configLocation = './sftp-config.json';
+var ignorePatterns = '';
+var host = '';
+var username = '';
+var password = '';
+var remotePath = '';
+var isMac = /^darwin/.test(process.platform);
+
+var start = function() {
+
+  console.log('Watching directory: ' + process.cwd());
+
+  // Create SFTP connection
+  var sftp = new SFTPS({
+    host: host, // required 
+    username: username, // required 
+    password: password, // required 
+    port: 22 // optional 
+  });
+
+  // Create scan function that runs & empties the SFTP queue every second
+  var scan = function() { 
+    if(sftp.cmds.length > 0) {
+      sftp.exec(function(err,res) {
+        if(err) throw err;
+        if(res.data) {
+          var numberOfItems = res.data.split('\n').length - 1;
+          console.log(colors.green('Succesfully uploaded ' + numberOfItems + ' file(s)'));
+        }
+      });
+    }
+    setTimeout(scan, scanInterval);
+  };
+  setTimeout(scan, scanInterval);
+
+  // Initiate the watcher
+  watch('.', function(filename) {
+    
+    // See if it matches 'ignore_regexes'
+    var matches = false;
+    for(var i=0;i<ignorePatterns.length;i++) {
+      var res = filename.match(ignorePatterns[i]);
+      if(res) {
+        matches = true;
+        break;
+      }
+    }
+
+    // Upload if it doesn't match the ignorePatterns
+    if(!matches) {
+      
+      var exists = fs.existsSync('./' + filename);
+      var destination = remotePath + '/' + filename;
+       
+      if(exists) {
+        console.log('Change detected in ' + filename + '. Uploading to -> ' + destination);
+        sftp.put('./' + filename, destination);
+      } else {
+        console.log('Delete detected on ' + filename + '. Deleting server file -> ' + destination);
+        sftp.rm(destination);
+      }
+
+    }
+
+  });
+}
 
 // Clear console screen
 util.print("\u001b[2J\u001b[0;0H");
@@ -17,75 +86,99 @@ util.print("\u001b[2J\u001b[0;0H");
 console.log(colors.blue('Sublime SFTP directory watcher v' + settings.version));
 
 // Try to read sftp-config.json file
-try {
-  content = fs.readFileSync('./sftp-config.json','utf-8');
-} catch(e) {
-  console.log(colors.red('Error: sftp-config.json not found!'));
-  process.exit();
-}
-
-// Try to parse sftp-config.json file
-try{
-  var config = RJSON.parse(content);
-} catch(e) {
-  console.log(colors.red('Error: Unable to parse sftp-config.json!'));
-  process.exit();
-}
-
-console.log('Watching directory: ' + process.cwd());
-
-var ignorePatterns = config.ignore_regexes;
-
-// Create SFTP connection
-var sftp = new SFTPS({
-  host: config.host, // required 
-  username: config.user, // required 
-  password: config.password, // required 
-  port: 22 // optional 
-});
-
-// Create scan function that runs & empties the SFTP queue every second
-var scan = function() { 
-  if(sftp.cmds.length > 0) {
-    sftp.exec(function(err,res) {
-      if(err) throw err;
-      if(res.data) {
-        var numberOfItems = res.data.split('\n').length - 1;
-        console.log(colors.green('Succesfully uploaded ' + numberOfItems + ' file(s)'));
-      }
-    });
-  }
-  setTimeout(scan, scanInterval);
-};
-setTimeout(scan, scanInterval);
-
-// Initiate the watcher
-watch('.', function(filename) {
-  
-  // See if it matches 'ignore_regexes'
-  var matches = false;
-  for(var i=0;i<ignorePatterns.length;i++) {
-    var res = filename.match(ignorePatterns[i]);
-    if(res) {
-      matches = true;
-      break;
-    }
-  }
-
-  // Upload if it doesn't match the ignorePatterns
-  if(!matches) {
+if(fs.existsSync(configLocation)) {
+  content = fs.readFileSync(configLocation,'utf-8');
+  // Try to parse sftp-config.json file
+  try{
+    var config = RJSON.parse(content);
+    host = config.host;
+    username = config.user;
+    password = config.password;
+    ignorePatterns = config.ignore_regexes;
+    remotePath = config.remote_path;
     
-    var exists = fs.existsSync('./' + filename);
-    var destination = config.remote_path + '/' + filename;
-     
-    if(exists) {
-      console.log('Change detected in ' + filename + '. Uploading to -> ' + destination);
-      sftp.put('./' + filename, destination);
+    // If password is set in config file (Like in Sublime) then use that
+    if(config.password) {
+      password = config.password;
+      start();
+    // Otherwise on Mac check the keychain for the password
+    } else if(isMac) {
+      var serviceName = 'sublime-sync-' + host + '-' + username;
+      keychain.getPassword({ account: 'foo', service: serviceName }, function(err, pass) {
+        password = pass;
+        start();
+        // Prints: Password is baz
+      });
     } else {
-      console.log('Delete detected on ' + filename + '. Deleting server file -> ' + destination);
-      sftp.rm(destination);
+      console.log(colors.red('Error: Unable to retrieve passwrod from sftp-config.json or keychain!'));
+      process.exit();
+    }
+    
+  } catch(e) {
+    console.log(colors.red('Error: Unable to parse sftp-config.json!'));
+    process.exit();
+  }
+} else {
+  console.log('No sftp-config.json found. Manually create file');
+  prompt.start({
+    'message': 'Please enter',
+  });
+
+  var schema = {
+    properties: {
+      host: {
+        description: "Host",
+        required: true
+      },
+      username: {
+        description: "Username",
+        required: true
+      },
+      password: {
+        description: "Password",
+        required: true,
+        hidden: true
+      },
+      remotePath: {
+        description: "Remote path",
+        required: true
+      }
+    }
+  };
+
+  prompt.get(schema, function (err, result) {
+   
+    var obj = {
+      host: result.host,
+      user: result.username,
+     // password: result.password,
+      remote_path: result.remotePath,
+      ignore_regexes: [
+          "\\.sublime-(project|workspace)", "sftp-config.json","node_modules","WEB-INF","web.config","bin",
+          "sftp-settings\\.json", "/venv/", "\\.svn/", "\\.hg/", "\\.git/", ".sass-cache",
+          "\\.bzr", "_darcs", "CVS", "\\.DS_Store", "Thumbs\\.db", "desktop\\.ini"
+      ]
     }
 
-  }
+    host = result.host;
+    username = result.username;
+    password = result.password;
+    remotePath = result.remotePath;
 
-});
+    fs.writeFileSync('./sftp-config.json',JSON.stringify(obj,null,4),{ encoding: 'utf8'});
+
+    // If it's OSX: Set password in the kechain stead of the file
+    if(isMac) {
+      var serviceName = 'sublime-sync-' + host + '-' + username;
+      console.log('Storing password in keychain under key: ' + serviceName);
+      keychain.setPassword({ account: 'foo', service: serviceName, password: password }, function(err) {
+        if(err) throw err;
+        start();
+      });
+    } else {
+      start();
+    }
+  
+  });
+
+}
